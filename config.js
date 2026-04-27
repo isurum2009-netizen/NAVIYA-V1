8c504ffa6975e4a420ed5b5d567052e89d108394
isurum2009-netizen <isuru.m2009@gmail.com>
2026-04-28 01:30:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/isurum2009-netizen/NAVIYA-V1/blob/main/images/IMG-20260408-WA0000.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 NAVIYA-XS Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
